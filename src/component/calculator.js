f9b49c72993ed63a815bd13a0ffb8fe76efa455d
AmanWorku <amexworku2014@gmail.com>
2023-01-12 12:33:16 +0300

    Remove commented section and fix linter errors

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -7,14 +7,14 @@ function Calculator() {
     total: null,
     next: null,
     operation: null,
-		  });
+  });
 
-		  const handleClick = (event) => {
+  const handleClick = (event) => {
     setState((prevState) => ({
       prevState, ...calculate(prevState, event.target.textContent),
     }));
-		  };
-		  const { total, next, operation } = state;
+  };
+  const { total, next, operation } = state;
   return (
     <div className="calculator">
       <div className="display-section">
@@ -87,96 +87,4 @@ function Calculator() {
     </div>
   );
 }
-// class Calculator extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(event) {
-//     const data = calculate(this.state, event.target.textContent);
-//     this.setState(data);
-//   }
-
-//   render() {
-//     const { total, next, operation } = this.state;
-//     return (
-//       <div className="calculator">
-//         <div className="display-section">
-//           <p className="display-value">
-//             {total}
-//             {' '}
-//             {operation}
-//             {' '}
-//             {next}
-//           </p>
-//         </div>
-
-//         <button type="button" onClick={this.handleClick}>
-//           AC
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           +/-
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           %
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="operator">
-//           ÷
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           7
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           8
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           9
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="operator">
-//           x
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           4
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           5
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           6
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="operator">
-//           -
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           1
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           2
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           3
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="operator">
-//           +
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="zero">
-//           0
-//         </button>
-//         <button type="button" onClick={this.handleClick}>
-//           .
-//         </button>
-//         <button type="button" onClick={this.handleClick} className="operator">
-//           =
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
 export default Calculator;
